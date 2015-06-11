9a8f5aa45e81065775a13de4c40ada03b40708d4
Jim Killingsworth <jkillingsworth@gmail.com>
2015-06-11 03:17:27 -0400

    Add epsilon greedy example

---
+++ chapter02/Example-EpsilonGreedy/Chart.fs
@@ -0,0 +1,98 @@
+﻿module Chart
+
+open OxyPlot
+open OxyPlot.Axes
+open OxyPlot.Series
+
+//-------------------------------------------------------------------------------------------------
+
+let private convertToDataPoint i x = DataPoint(double i, x)
+
+let private exportToPng model path w h =
+
+    use writeStream = System.IO.File.OpenWrite(path)
+    let pngExporter = OxyPlot.WindowsForms.PngExporter()
+    pngExporter.Width <- w
+    pngExporter.Height <- h
+    pngExporter.Export(model, writeStream)
+
+//-------------------------------------------------------------------------------------------------
+
+let renderAverageReward path data =
+
+    let model = PlotModel()
+
+    model.LegendBackground <- OxyColors.White
+    model.LegendBorder <- OxyColors.Black
+    model.LegendBorderThickness <- 1.0
+    model.LegendPlacement <- LegendPlacement.Inside
+    model.LegendPosition <- LegendPosition.RightBottom
+    model.PlotMargins <- OxyThickness(nan, nan, 10.0, nan)
+
+    let axis = new LinearAxis(Title = "Steps")
+    axis.Position <- AxisPosition.Bottom
+    axis.Minimum <- 0.0
+    axis.Maximum <- 1000.0
+    axis.MajorStep <- 250.0
+    axis.MinorStep <- 50.0
+    model.Axes.Add(axis)
+
+    let axis = LinearAxis(Title = "Average Reward")
+    axis.Position <- AxisPosition.Left
+    axis.Minimum <- 0.0
+    axis.Maximum <- 1.5
+    axis.MajorStep <- 0.5
+    axis.MinorStep <- 0.25
+    axis.AxisTitleDistance <- 14.0
+    model.Axes.Add(axis)
+
+    for item in data do
+        let series = LineSeries(Title = sprintf "ε = %.2f" (fst item))
+        series.StrokeThickness <- 1.0
+        model.Series.Add(series)
+        snd item
+        |> Array.mapi convertToDataPoint
+        |> Array.iter series.Points.Add
+
+    exportToPng model path 700 400
+
+//-------------------------------------------------------------------------------------------------
+
+let renderOptimalAction path data =
+
+    let model = PlotModel()
+
+    model.LegendBackground <- OxyColors.White
+    model.LegendBorder <- OxyColors.Black
+    model.LegendBorderThickness <- 1.0
+    model.LegendPlacement <- LegendPlacement.Inside
+    model.LegendPosition <- LegendPosition.RightBottom
+    model.PlotMargins <- OxyThickness(nan, nan, 10.0, nan)
+
+    let axis = new LinearAxis(Title = "Steps")
+    axis.Position <- AxisPosition.Bottom
+    axis.Minimum <- 0.0
+    axis.Maximum <- 1000.0
+    axis.MajorStep <- 250.0
+    axis.MinorStep <- 50.0
+    model.Axes.Add(axis)
+
+    let axis = LinearAxis(Title = "Optimal Action")
+    axis.Position <- AxisPosition.Left
+    axis.Minimum <- 0.0
+    axis.Maximum <- 1.0
+    axis.MajorStep <- 0.2
+    axis.MinorStep <- 0.1
+    axis.StringFormat <- "P0"
+    axis.AxisTitleDistance <- 14.0
+    model.Axes.Add(axis)
+
+    for item in data do
+        let series = LineSeries(Title = sprintf "ε = %.2f" (fst item))
+        series.StrokeThickness <- 1.0
+        model.Series.Add(series)
+        snd item
+        |> Array.mapi convertToDataPoint
+        |> Array.iter series.Points.Add
+
+    exportToPng model path 700 400
---
+++ chapter02/Example-EpsilonGreedy/Compute.fs
@@ -0,0 +1,80 @@
+﻿module Compute
+
+open MathNet.Numerics.Distributions
+
+//-------------------------------------------------------------------------------------------------
+
+let tasks = 2000
+let steps = 1000
+let n = 10
+
+//-------------------------------------------------------------------------------------------------
+
+type State =
+    { ActionValuesActual : double[]
+      ActionValuesApprox : double[]
+      AccumulatedRewards : double[]
+      AccumulatedSelects : int[] }
+
+type Value =
+    { Reward : double
+      OptimalActionTaken : bool }
+
+type Result =
+    { AverageReward : double
+      OptimalAction : double }
+
+//-------------------------------------------------------------------------------------------------
+
+let selectOptimalAction actionValues =
+    actionValues
+    |> Array.mapi (fun i x -> i, x)
+    |> Array.maxBy snd
+    |> fst
+
+let selectAction epsilon random state =
+    match (Sample.continuousUniform 0.0 1.0 random) with
+    | x when x < epsilon
+        -> Sample.discreteUniform 0 (n - 1) random
+    | _ -> selectOptimalAction state.ActionValuesApprox
+
+let executeOneStep epsilon random state =
+
+    let actionOptimal = selectOptimalAction state.ActionValuesActual
+    let action = selectAction epsilon random state
+    let reward = state.ActionValuesActual.[action] + (Sample.normal 0.0 1.0 random)
+
+    state.AccumulatedRewards.[action] <- state.AccumulatedRewards.[action] + reward
+    state.AccumulatedSelects.[action] <- state.AccumulatedSelects.[action] + 1
+    state.ActionValuesApprox.[action] <- state.AccumulatedRewards.[action] / double state.AccumulatedSelects.[action]
+
+    { Reward = reward; OptimalActionTaken = action = actionOptimal }, state
+
+let executeOneTask epsilon random _ =
+
+    let state =
+        { ActionValuesActual = Array.init n (fun i -> Sample.normal 0.0 1.0 random)
+          ActionValuesApprox = Array.create n 0.0
+          AccumulatedRewards = Array.create n 0.0
+          AccumulatedSelects = Array.create n 0 }
+
+    state
+    |> Seq.unfold (executeOneStep epsilon random >> Some)
+    |> Seq.take steps
+    |> Seq.toArray
+
+let computeAverageResults (valuesByTask : Value[][]) =
+
+    let computeAverage step selection =
+        valuesByTask |> Seq.averageBy (fun values -> selection values.[step])
+
+    let computeAverageResult step =
+        { AverageReward = computeAverage step (fun x -> x.Reward)
+          OptimalAction = computeAverage step (fun x -> if x.OptimalActionTaken then 1.0 else 0.0) }
+
+    Array.init steps computeAverageResult
+
+let computeResults epsilon random =
+    executeOneTask epsilon random
+    |> Array.init tasks
+    |> computeAverageResults
